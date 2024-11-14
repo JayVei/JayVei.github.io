d2cd2fb03f2e230f6341bdc0a4ecbb8597e09d94
jayvei <2060521423@qq.com>
2024-11-14 17:52:08 +0800

    Site updated: 2024-11-14 17:52:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/09/Coding/","2024/11/09/Coding2/","2024/11/09/Coding3/","2024/11/09/Hello-Hexo/","2024/11/09/hello-world/","2024/11/14/MarkDown语法/","2024/11/14/htmlNote/"];function toRandomPost(){
+var posts=["2024/11/09/Coding/","2024/11/09/Coding3/","2024/11/09/Coding2/","2024/11/09/Hello-Hexo/","2024/11/14/MarkDown语法/","2024/11/09/hello-world/","2024/11/14/htmlNote/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
