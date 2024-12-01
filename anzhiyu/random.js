b1bfc3de6999a8e67f985b99258774092ce8c89f
jayvei <2060521423@qq.com>
2024-12-01 16:38:27 +0800

    Site updated: 2024-12-01 16:38:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["11/09/93e553a599a1/","11/09/51e8ef0867d0/","11/09/6e2c5839bc02/","11/09/ebdfca3e3520/","11/09/4984c732e544/","11/14/d86bfd4a2197/","11/14/1e31d8e29434/","11/27/17ab213a0fb5/","11/27/162764bc221d/","11/27/b09837e61875/"];function toRandomPost(){
+var posts=["11/09/51e8ef0867d0/","11/09/ebdfca3e3520/","11/09/93e553a599a1/","11/09/4984c732e544/","11/09/6e2c5839bc02/","11/14/d86bfd4a2197/","11/14/1e31d8e29434/","11/27/162764bc221d/","11/27/17ab213a0fb5/","11/27/b09837e61875/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
